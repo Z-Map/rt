d53d819d6fcb6b9bb106934b3d7adcdf8e3d7f13
Quentin <blendfan@gmail.com>
2017-03-19 21:03:44 +0100

    update data structure

--- include/data/rt_data_tree.h
+++ include/data/rt_data_tree.h
@@ -1,36 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rt_data_objects.h                                  :+:      :+:    :+:   */
+/*   rt_data_tree.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/16 18:56:24 by qloubier          #+#    #+#             */
-/*   Updated: 2017/03/19 19:38:23 by qloubier         ###   ########.fr       */
+/*   Created: 2017/03/19 16:59:57 by qloubier          #+#    #+#             */
+/*   Updated: 2017/03/19 19:44:29 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RT_DATA_OBJECTS
-#define RT_DATA_OBJECTS
+#ifndef RT_DATA_TREE_H
+# define RT_DATA_TREE_H
 
 # include "mathex/vector.h"
 # include "mathex/matrix.h"
 # include "rt_prototype.h"
 
-struct				s_rt_object_instance
+struct				s_rt_treenode
 {
-	unsigned int	id;
-	int				flags;
-	char			*name;
-	t_mattf			transform;
-	t_rtobj			*obdata;
+	t_rtnode		*parent;
+	t_rtnode		*next;
+	t_rtnode		*childs;
+	unsigned int	type;
+	unsigned int	flags;
+	t_rtobi			*instance;
 };
 
-struct				s_rt_object
+struct				s_rt_tree
 {
-	t_rtobt			type:32;
-	unsigned int	flags;
-	char			*name;
+	t_rtnode		node;
+	unsigned int	max_depth;
+	unsigned int	nodelen;
+	t_blst			*buffer;
 };
+
+struct				s_rt_rtreenode
+{
+	t_rtnode		node;
+	t_mattf			invert_transform;
+
+};
+
 
 #endif
--- include/rt_prototype.h
+++ include/rt_prototype.h
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/23 05:31:18 by qloubier          #+#    #+#             */
-/*   Updated: 2017/03/19 14:38:54 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/03/19 17:32:21 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,22 @@ typedef struct s_rt_object			t_rtobj;
 
 typedef enum						e_rt_object_type
 {
-	ROOT = 1,
-	CUBE = 1 << 1,
-	PLAN = 1 << 2,
-	SPHERE = 1 << 3,
-	CAMERA = 1 << 4,
-	EMPTY = 1 << 5,
-	SPOT = 1 << 6,
-	POINTLIGHT = 1 << 7,
-	FACE = 1 << 8,
-	INVALID = 1 << 9,
-	CONE = 1 << 10,
-	CYL = 1 << 11,
-	SUNLIGHT = 1 << 12,
-	SETTING = 1 << 13,
-	CONE_INF = 1 << 14,
-	NOSHADER = 1 << 28,
-	TRIANGLE = 1 << 29,
-	MESH = 1 << 30,
-	RADIUS = SPHERE | CUBE | CYL | CONE | CONE_INF,
-	VISIBLE = CUBE | PLAN | SPHERE | FACE | CONE | CYL | CONE_INF |
-	TRIANGLE | MESH,
-	NOCHECKBOX = PLAN | CYL | CONE_INF,
-	BOUNDED = CUBE | SPHERE | FACE | CONE | CYL | CONE_INF | TRIANGLE |
-	EMPTY,
-	LIGHTTYPE = SPOT | POINTLIGHT | SUNLIGHT,
+	INVALID		= 0,
+	ROOT		= 0x1,
+	EMPTY		= 0x2,
+	CAMERA		= 0x4,
+	PLAN		= 0x8,
+	SPHERE		= 0x10,
+	CONE		= 0x20,
+	CYLINDER	= 0x40,
+	CUBE		= 0x80,
+	TRIS		= 0x100,
+	MESH		= 0x200,
+	SPOT		= 0x400,
+	POINTLIGHT	= 0x800,
+	SUNLIGHT	= 0x1000,
+	VISIBLE		= PLAN | SPHERE | CONE | CYLINDER | CUBE | TRIS | MESH,
+	LIGHT		= SPOT | POINTLIGHT | SUNLIGHT,
 }									t_rtobt;
 
 
