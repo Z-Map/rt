dc38523c6ca89bec2792b2ecda1368be79a3b3e5
Lise CARREEL <lcarreel@student.42.fr>
2017-04-12 16:23:13 +0200

    tree - tree_setparent + correction tree_delchild

--- src/tree/tree_setparent.c
+++ src/tree/tree_setparent.c
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tree_delchild.c                                    :+:      :+:    :+:   */
+/*   tree_setparent.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcarreel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/10 18:05:08 by lcarreel          #+#    #+#             */
-/*   Updated: 2017/04/12 16:22:33 by lcarreel         ###   ########.fr       */
+/*   Created: 2017/04/12 16:16:10 by lcarreel          #+#    #+#             */
+/*   Updated: 2017/04/12 16:22:12 by lcarreel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt_tree.h"
 
-t_rtnode		*tree_delchild(t_rtnode *parent, t_rtnode *node)
+t_rtnode		*tree_setparent(t_rtnode *node, t_rtnode *parent)
 {
-	t_rtnode	**tmp;
-
-	tmp = &(parent->childs);
 	if (!parent || !node)
 		return (NULL);
-	while (*tmp && (*tmp != node))
-		tmp = &((*tmp)->next);
-	if (!*tmp)
-		return (NULL);
-	*tmp = node->next;
-	node->next = NULL;
-	node->parent = NULL;
+	if (node->parent)
+		tree_delchild(node->parent, node);
+	node->parent = parent;
+	node->next = parent->childs;
+	parent->childs = node;
 	return (node);
 }
