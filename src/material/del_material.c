57feecdf583f6a9a6167af845b1cc13c053e17bb
Lise CARREEL <lcarreel@student.42.fr>
2017-04-25 18:57:11 +0200

    create and delete materials

---
+++ src/material/del_material.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   del_material.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcarreel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/25 18:41:48 by lcarreel          #+#    #+#             */
+/*   Updated: 2017/04/25 18:41:54 by lcarreel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt_object.h"
+#include "rt_data_material.h"
+
+int				rmmaterial(t_rtmat *mat)
+{
+	if (mat)
+	{
+		ft_vfree(mat->name);
+		ft_vfree(mat);
+	}
+	else
+		return (0);
+	return (1);
+}
+
+int				rmmaterials(size_t num, ...);
+{
+	va_list		mats;
+	size_t		i;
+	int			ret;
+
+	va_start(mats, num);
+	i = 0;
+	ret = 1;
+	while (i < num)
+	{
+		if (!rmmaterial(va_arg(mats, t_rtmat *)))
+			ret = 0;
+	}
+	va_end(mats);
+	return (ret);
+}
---
+++ src/material/new_material.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   new_material.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcarreel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/25 17:59:47 by lcarreel          #+#    #+#             */
+/*   Updated: 2017/04/25 17:59:48 by lcarreel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt_object.h"
+#include "rt_data_material.h"
+
+t_rtmat				*mkmaterial(const char *name)
+{
+	static t_vmem	*vm;
+	t_rtmat			*newmat;
+
+	if (!vm)
+		vm = ft_vmemnew((sizeof(t_rtmat) + sizeof(t_vmps)) * RTMAT_MEMBUF_SIZE);
+	if (!(newmat = (t_rtmat *)ft_vmemalloc(vm, sizeof(t_rtmat))))
+		return (NULL);
+	newmat = (t_rtmat *){.name = ft_vsdup(name),
+		.color1 = (t_rgba){0, 0, 0, 0}, .color2 = (t_rgba){0, 0, 0, 0},
+	 	.reflectivity = 0, .refraction = 0};
+	return (newmat);
+}
+
+int					mkobjects(t_rtobj **mattab, size_t num, ...)
+{
+	va_list			mats;
+	size_t			i;
+
+	va_start(mats, num);
+	i = 0;
+	while (i < num)
+	{
+		if (!(mattab[i] = mkmaterial(va_arg(mats, char *))))
+			break;
+		i++;
+	}
+	va_end(mats);
+	if (i != num)
+	{
+		while (i)
+			rmmaterials(mattab[i--]);
+		return (0);
+	}
+	return (1);
+}
