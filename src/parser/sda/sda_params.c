9f4c844932eb7804b62f2aeb4830199bbc1401a3
Lise Carreel <lcarreel@outlook.com>
2017-05-01 17:26:59 +0200

    sda_param corrections

--- src/parser/sda/sda_params.c
+++ src/parser/sda/sda_params.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "rt_object.h"
 #include "data_sda.h"
 
 /*
@@ -22,6 +23,11 @@
 ** Check si la ligne correspond à quelque chose de connu.
 ** Si le mot est inconnu au parser, on appellera une fonction d'erreur.
 */
+
+static char	*get_name(char *s)
+{
+
+}
 
 static int	make_new_node(t_rtnode *node, t_rtobt type, char *s)
 {
@@ -50,28 +56,28 @@ static int	check_line2(t_rtnode *node, char *s)
 int			check_line(t_rtnode *node, char *s)
 {
 	if (!ft_strcmp(s, "EMPTY"))
-		return (make_new_node(node, EMPTY));
+		return (make_new_node(node, EMPTY, s));
 	else if (!ft_strcmp(s, "CAMERA"))
-		return (make_new_node(node, CAMERA));
+		return (make_new_node(node, CAMERA, s));
 	else if (!ft_strcmp(s, "PLAN"))
-		return (make_new_node(node, PLAN));
+		return (make_new_node(node, PLAN, s));
 	else if (!ft_strcmp(s, "SPHERE"))
-		return (make_new_node(node, SPHERE));
+		return (make_new_node(node, SPHERE, s));
 	else if (!ft_strcmp(s, "CONE"))
-		return (make_new_node(node, CONE));
+		return (make_new_node(node, CONE, s));
 	else if (!ft_strcmp(s, "CYLINDER"))
-		return (make_new_node(node, CYLINDER));
+		return (make_new_node(node, CYLINDER, s));
 	else if (!ft_strcmp(s, "CUBOID"))
-		return (make_new_node(node, CUBOID));
+		return (make_new_node(node, CUBOID, s));
 	else if (!ft_strcmp(s, "MESH"))
-		return (make_new_node(node, MESH));
+		return (make_new_node(node, MESH, s));
 	else if (!ft_strcmp(s, "TRIS"))
-		return (make_new_node(node, TRIS));
+		return (make_new_node(node, TRIS, s));
 	else if (!ft_strcmp(s, "SPOT"))
-		return (make_new_node(node, SPOT));
+		return (make_new_node(node, SPOT, s));
 	else if (!ft_strcmp(s, "POINTLIGHT"))
-		return (make_new_node(node, POINTLIGHT));
+		return (make_new_node(node, POINTLIGHT, s));
 	else if (!ft_strcmp(s, "SUNLIGHT"))
-		return (make_new_node(node, SUNLIGHT));
+		return (make_new_node(node, SUNLIGHT, s));
 //	return (check_line2(node, s));
 }
