45f00cbf0689a9bfe9dd105ed1c1d9f89f33656b
Lise CARREEL <lcarreel@student.42.fr>
2017-05-09 19:15:20 +0200

    render thread corrected

--- src/thread/rdrmgr_thread.c
+++ src/thread/rdrmgr_thread.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/23 01:42:02 by qloubier          #+#    #+#             */
-/*   Updated: 2017/04/26 16:04:35 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/05/09 19:14:41 by lcarreel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ int			rt_init_rdrmgrthread(t_rt *rt)
 {
 	rt->render.refresh_lock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
 	rt->render.refresh_cond = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-	if (pthread_create(&(rt->render.mgrthread), NULL, &rdrmgr_main, rt))
+	if (pthread_create(&(rt->render.mgrthread), NULL, &rt_rdrmgr_main, rt))
 		return (rt_error(rt, 122, "Unable to start render thread."));
 	return (1);
 }
 
 int			rt_sync_rdrmgrthread(t_rt *rt)
 {
-	
+	rt = NULL; // A ENLEVER
+	return (1);
 }
