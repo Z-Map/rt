4cfa76f9ed66ebbf39c75dee433a4b8c0924ef70
Lise CARREEL <lcarreel@student.42.fr>
2017-05-15 18:21:34 +0200

    args - replace initialisation NULL by (void)variable

--- src/parser/args.c
+++ src/parser/args.c
@@ -6,7 +6,7 @@
 /*   By: lcarreel <lcarreel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 16:42:15 by lcarreel          #+#    #+#             */
-/*   Updated: 2017/05/14 20:56:36 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/05/15 18:20:34 by lcarreel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int			arg_commandmode(const char *arg, void *env,
 {
 	t_rt	*rt;
 
-	arg = NULL; // A ENLEVER
-	nextargtab = NULL; // A ENLEVER
-	tab_len = 0; // A ENLEVER
+	(void)arg;
+	(void)nextargtab;
+	(void)tab_len;
 	rt = (t_rt *)env;
 	rt->flags |= RT_COMMANDMODE;
 	rt->flags &= ~RT_CLIENTMODE;
@@ -31,7 +31,7 @@ int			arg_fileout(const char *arg, void *env,
 {
 	t_rt	*rt;
 
-	arg = NULL; // A ENLEVER
+	(void)arg;
 	rt = (t_rt *)env;
 	if (tab_len)
 		rt->fileout = ft_strdup(nextargtab[0]);
@@ -45,9 +45,9 @@ int			arg_clientmode(const char *arg, void *env,
 {
 	t_rt	*rt;
 
-	arg = NULL; // A ENLEVER
-	nextargtab = NULL; // A ENLEVER
-	tab_len = 0; // A ENLEVER
+	(void)arg;
+	(void)nextargtab;
+	(void)tab_len;
 	rt = (t_rt *)env;
 	rt->flags = (rt->flags & ~RT_MODES) | RT_CLIENTMODE;
 	return (0);
@@ -58,9 +58,9 @@ int			arg_visualprev(const char *arg, void *env,
 {
 	t_rt	*rt;
 
-	arg = NULL; // A ENLEVER
-	nextargtab = NULL; // A ENLEVER
-	tab_len = 0; // A ENLEVER
+	(void)arg;
+	(void)nextargtab;
+	(void)tab_len;
 	rt = (t_rt *)env;
 	rt->flags |= RT_VISUALPREV | ~RT_CLIENTMODE;
 	return (0);
@@ -71,9 +71,9 @@ int			arg_modes(const char *arg, void *env,
 {
 	t_rt	*rt;
 
-	arg = NULL; // A ENLEVER
-	nextargtab = NULL; // A ENLEVER
-	tab_len = 0; // A ENLEVER
+	(void)arg;
+	(void)nextargtab;
+	(void)tab_len;
 	rt = (t_rt *)env;
 	rt->flags |= RT_MODES;
 	return (0);
