550a441626c6b6a6d9853416c0d8192608ad7c54
Lise Carreel <lcarreel@outlook.com>
2017-06-06 18:53:51 +0200

    debut parser textures

---
+++ src/parser/texture_parse.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   texture_parse.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcarreel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/10 18:05:08 by lcarreel          #+#    #+#             */
+/*   Updated: 2017/04/12 16:22:33 by lcarreel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_parse.h"
+
+/*static int	tp_parse(char *value, int len)
+{
+	
+}
+
+static int	ti_parse(char *name, int len)
+{
+	
+q
+
+int		texture_parse(t_val *val, void *mem, t_gparse parser)
+{
+	int	ret;
+
+	ret = 0;
+	if (!parser.value)
+		return (0);
+	ft_prstrim(&(parser.value), &(parser.v_len), FT_WHITESPACE);
+	if (ft_strnequ(parser.value, "tp_", 3))
+		ret = tp_parse(&(*(parser.value + 3)), parser.v_len - 3);
+	else
+		ret = ti_parse();
+	return (ret);
+}*/
