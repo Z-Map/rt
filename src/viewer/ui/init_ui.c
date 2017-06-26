cf839f26d088bac7e1d809902fb9349802dea7c3
lapiquette <helsonalexis@gmx.com>
2017-06-26 21:52:49 +0200

    [+] improove hit function - fix bug

--- src/viewer/ui/init_ui.c
+++ src/viewer/ui/init_ui.c
@@ -41,18 +41,24 @@ t_rtnode			*init_ui(t_v2i dim)
 	//	return (0);
 
 	//tets laodbar
-	//if (!(tmp = tree_addchild(node,\
-	//mknlayer_loadbar((t_v2f){0.0, 0.0}, (t_v2f){0.6, 0.1}, color, PL_MIDDLE))))
-	//	return (0);
-        if (!(tmp = tree_addchild(node,\
-        mknlayer_variator((t_v2f){0.0, 0.0}, (t_v2f){0.6, 0.1}, color2, PL_MIDDLE))))
-                return (0);
-	/*
-        if (!(tree_addchild_wid(node, mknlayer_rect((t_v2f){0.0, 0.0}, (t_v2f){0.2, 0.8},\
-          (t_v4i){0, 0, 255, 200}, PL_MIDDLE), 1)))
-         	return (0);
+//	if (!(tmp = tree_addchild(node,\
+//	mknlayer_loadbar((t_v2f){0.0, 0.0}, (t_v2f){0.6, 0.1}, color, PL_BOTTOM))))
+//		return (0);
+
+	if (!(tmp = tree_addchild(node, mknlayer_variator((t_v2f){0.0, 0.0}, (t_v2f){0.6, 0.1}, color2, PL_MIDDLE))))
+		return (0);
+tmp =0;
+        if (!(tree_addchild(node, mknlayer_checkbox((t_v2f){0.0, 0.0}, (t_v2f){0.2, 0.2},\
+        	(t_v4i){0, 0, 255, 200}, PL_TOP))))
+        	return (0);
+
+
+	if (!(tree_addchild(node, mknlayer_checkbox((t_v2f){0.0, 0.2}, (t_v2f){0.2, 0.2},\
+		(t_v4i){0, 255, 0, 200}, PL_MIDDLE))))
+              return (0);
+	//printf(". : %d\n childs : %d\n childs->next : %d\n", node->flags, node->childs->flags, node->childs->next->flags);
 	//tets d ajout de bordure sur l image principal id : 3
-        if (!(tree_addchild_wid(node, mknlayer_border((t_v2f){0.0, 0.0}, (t_v2f){1.0, 1.0},\
+/*        if (!(tree_addchild_wid(node, mknlayer_border((t_v2f){0.0, 0.0}, (t_v2f){1.0, 1.0},\
  	      (t_v4i){0, 255, 0, 200}, PL_MIDDLE), 1)))
 		return (0);
 	*/
@@ -63,10 +69,10 @@ t_rtnode			*init_ui(t_v2i dim)
 	((t_layer_border *)(tmp->content))->valid_border = 4;*/
 
 	//creation de l element de droit 4 - 6
-	/*if (!(tmp = tree_addchild(node, mknlayer_rect((t_v2f){0.0, 0.0}, (t_v2f){0.2, 1.0},\
-	(t_v4i){200, 100, 100, 200}, PL_RIGHT))))
+	if (!(tmp = tree_addchild(node, mknlayer_rect((t_v2f){0.0, 0.0}, (t_v2f){1.0, 0.2},\
+	(t_v4i){200, 100, 100, 200}, PL_BOTTOM))))
 		return (0);
-	if (!(tree_addchild_wid(node, mknlayer_rect((t_v2f){0.0, 0.0}, (t_v2f){0.2, 0.8},\
+	/*if (!(tree_addchild_wid(node, mknlayer_rect((t_v2f){0.0, 0.0}, (t_v2f){0.2, 0.8},\
 	(t_v4i){0, 0, 255, 200}, PL_MIDDLE), 4)))
 		return (0);
 	//tets d ajout de bordure sur l image principal id : 3
--- src/viewer/ui/ui_found_elem_with_pos.c
+++ src/viewer/ui/ui_found_elem_with_pos.c
@@ -14,7 +14,46 @@
 #include "rt_tree.h"
 #include <stdio.h>
 
+/*
 void			ui_found_elem_with_pos(t_rtnode *node,\
+t_rtnode **found, t_v2i *pos_cursor)
+{
+	t_rtnode	*tmp;
+
+	if (*found)
+	{
+		printf("Ned ...\n");
+		return ;
+	}
+	tmp = node;
+	if (tmp)
+		printf("---> Actual id : %d\n", tmp->flags);
+	if (tmp->next)
+		printf("---> Next : %d.\n", tmp->next->flags);
+	if (tmp->childs)
+		printf("---> Childs : %d.\n", tmp->childs->flags);
+	if (tmp->next && (((t_layer_root *)(tmp->next->content))->gen.show) &&\
+	ui_check_hit(tmp->next, pos_cursor))
+	{
+		printf("Next --> %d:)\n", tmp->next->flags);
+		ui_found_elem_with_pos(tmp->next, found, pos_cursor);
+	}
+	if ((tmp->childs && (((t_layer_root *)(tmp->childs->content))->gen.show) &&\
+	ui_check_hit(tmp->childs, pos_cursor)) || (tmp->childs && tmp->childs->next && tmp->parent->type == TE_ROOT))
+	{
+		printf("Childs --> %d\n", tmp->childs->flags);
+		ui_found_elem_with_pos(tmp->childs, found, pos_cursor);
+	}
+	if (ui_is_event(((t_layer_root *)(tmp->content))->gen) && ui_check_hit(tmp, pos_cursor))
+	{
+		printf("HIt ...\n");
+		*found = tmp;
+	}
+	printf("\n\n");
+}
+*/
+
+void	ui_found_elem_with_pos(t_rtnode *node,\
 t_rtnode **found, t_v2i *pos_cursor)
 {
 	t_rtnode	*tmp;
@@ -22,12 +61,10 @@ t_rtnode **found, t_v2i *pos_cursor)
 	if (*found)
 		return ;
 	tmp = node;
-	if (tmp->next && (((t_layer_root *)(tmp->next->content))->gen.show) &&\
-	ui_check_hit(tmp->next, pos_cursor))
+	if (tmp->next)
 		ui_found_elem_with_pos(tmp->next, found, pos_cursor);
-	if (tmp->childs && (((t_layer_root *)(tmp->childs->content))->gen.show) &&\
-	ui_check_hit(tmp->childs, pos_cursor))
+	if (tmp->childs)
 		ui_found_elem_with_pos(tmp->childs, found, pos_cursor);
-	if (ui_is_event(((t_layer_root *)(tmp->content))->gen))
+	if (ui_is_event(((t_layer_root *)(tmp->content))->gen) && ui_check_hit(tmp, pos_cursor))
 		*found = tmp;
 }
