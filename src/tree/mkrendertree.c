9f38a51559777e7bcbc55b993a57bc77cac920ae
lapiquette <helsonalexis@gmx.com>
2017-06-28 17:57:01 +0200

    fix norm

--- src/tree/mkrendertree.c
+++ src/tree/mkrendertree.c
@@ -15,69 +15,71 @@
 #include "rt_object.h"
 #include "rt_tree.h"
 
-// static t_rtnode		*find_cam(t_rtnode *n)
-// {
-// 	t_rtnode		*c;
-//
-// 	if (!n)
-// 		return (NULL);
-// 	if (((t_rtobi *)n->content)->obj->type & CAMERA)
-// 		return (n);
-// 	if ((c = find_cam(n->childs)))
-// 		return (c);
-// 	else if ((c = find_cam(n->next)))
-// 		return (c);
-// 	return (NULL);
-// }
-
-// static t_rtnode	*rendernodechilds(t_rtnode *childs, t_mattf m, t_mat3x2f *b)
-// {
-// 	t_rtnode		*nchilds;
-// 	t_rtnode		**c;
-//
-// 	if (!childs)
-// 		return (NULL);
-// 	c = &nchilds;
-// 	while (c && childs)
-// 	{
-// 		if (!(*c = (t_rtnode *)rendernodedup(childs, m, b)))
-// 			c = NULL;
-// 		else
-// 			c = &((*c)->next);
-// 		childs = childs->next;
-// 	}
-// 	if (!c)
-// 	{
-// 		while (nchilds)
-// 		{
-// 			childs = nchilds->next;
-// 			rmnode(&nchilds);
-// 			nchilds = childs;
-// 		}
-// 	}
-// 	return (nchilds);
-// }
-//
-// t_rtrnode		*rendernodedup(t_rtnode *node, t_mattf m, t_mat3x2f *b)
-// {
-// 	t_rtrnode	*rnode;
-// 	t_mat3x2f	lb;
-//
-// 	if (!node)
-// 		return (NULL);
-// 	pmattf_multiply(&m, &(((t_rtobi *)(node->content))->transform));
-// 	lb = instance_getbound((t_rtobi *)(node->content), &m);
-// 	rnode = mkrendernode((t_rtobi *)(node->content), m, lb);
-// 	if (node->childs && (!(rnode->node.childs = rendernodechilds(
-// 		node->childs, m, &lb))))
-// 		rmnode((t_rtnode **)&rnode);
-// 	else
-// 	{
-// 		((t_rtrnode *)rnode)->bound = lb;
-// 		*b = bound_union(lb, *b);
-// 	}
-// 	return (rnode);
-// }
+/*
+** static t_rtnode		*find_cam(t_rtnode *n)
+** {
+** 	t_rtnode		*c;
+**
+** 	if (!n)
+** 		return (NULL);
+** 	if (((t_rtobi *)n->content)->obj->type & CAMERA)
+** 		return (n);
+** 	if ((c = find_cam(n->childs)))
+** 		return (c);
+** 	else if ((c = find_cam(n->next)))
+** 		return (c);
+** 	return (NULL);
+** }
+**
+** static t_rtnode	*rendernodechilds(t_rtnode *childs, t_mattf m, t_mat3x2f *b)
+** {
+** 	t_rtnode		*nchilds;
+** 	t_rtnode		**c;
+**
+** 	if (!childs)
+** 		return (NULL);
+** 	c = &nchilds;
+** 	while (c && childs)
+** 	{
+** 		if (!(*c = (t_rtnode *)rendernodedup(childs, m, b)))
+** 			c = NULL;
+** 		else
+** 			c = &((*c)->next);
+** 		childs = childs->next;
+** 	}
+** 	if (!c)
+** 	{
+** 		while (nchilds)
+** 		{
+** 			childs = nchilds->next;
+** 			rmnode(&nchilds);
+** 			nchilds = childs;
+** 		}
+** 	}
+** 	return (nchilds);
+** }
+**
+** t_rtrnode		*rendernodedup(t_rtnode *node, t_mattf m, t_mat3x2f *b)
+** {
+** 	t_rtrnode	*rnode;
+** 	t_mat3x2f	lb;
+**
+** 	if (!node)
+** 		return (NULL);
+** 	pmattf_multiply(&m, &(((t_rtobi *)(node->content))->transform));
+** 	lb = instance_getbound((t_rtobi *)(node->content), &m);
+** 	rnode = mkrendernode((t_rtobi *)(node->content), m, lb);
+** 	if (node->childs && (!(rnode->node.childs = rendernodechilds(
+** 		node->childs, m, &lb))))
+** 		rmnode((t_rtnode **)&rnode);
+** 	else
+** 	{
+** 		((t_rtrnode *)rnode)->bound = lb;
+** 		*b = bound_union(lb, *b);
+** 	}
+** 	return (rnode);
+** }
+*/
 
 static void		*clean(t_rtnode *node)
 {
