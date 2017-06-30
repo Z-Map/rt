9200d786ab9fc04351fe3581e996f5ed47fb3cf0
lapiquette <helsonalexis@gmx.com>
2017-06-30 17:08:00 +0200

    [+]fsd

--- src/viewer/ui_display/draw_label.c
+++ src/viewer/ui_display/draw_label.c
@@ -12,15 +12,12 @@
 
 #include "rt_ui_display.h"
 #include "mathex/vector.h"
-#include <stdio.h>
 
 void	draw_label(mglwin *img, t_layer_label *label)
 {
 	float pos[2];
 
-	printf("Label ...\n");
 	pos[0] = label->gen.dim.x;
 	pos[1] = label->gen.dim.y;
 	mgl_drawmglstr(img, label->data, pos, 10, 0xFF00FF64);
-	printf("End ...\n");
 }
