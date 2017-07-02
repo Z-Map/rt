fcb9fd4470137ba85f98d0ba1025ae36c7138dd9
Quentin <blendfan@gmail.com>
2017-07-02 17:20:11 +0200

    add ambient ligt

--- src/raytracing/shaders/lights_loop.c
+++ src/raytracing/shaders/lights_loop.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 13:21:20 by qloubier          #+#    #+#             */
-/*   Updated: 2017/06/18 19:02:15 by ealbert          ###   ########.fr       */
+/*   Updated: 2017/07/02 17:14:54 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ t_v3f			shade_lightsloop(t_rtrgd geo, t_rdrtree *tree)
 {
 	size_t		i;
 	t_v3f		intensity;
+	t_v3f		al;
+	static int	gu = 0;
 
 	i = tree->lights_len;
 	intensity = (t_v3f){.x = 0.0f, .y = 0.0f, .z = 0.0f};
@@ -37,5 +39,11 @@ t_v3f			shade_lightsloop(t_rtrgd geo, t_rdrtree *tree)
 	while (i--)
 		pv3faddv3f(&intensity,
 			typelight(geo, (t_rtrnode *)(tree->lights[i]), tree));
+	al = rgbatov3f(((t_rtobd *)(tree->tree.scene))->scene.ambient_light);
+	if (gu++ < 6)
+		ft_printf("%v3f\n", &al);
+	intensity.x = mxmaxf(intensity.x, al.x);
+	intensity.y = mxmaxf(intensity.y, al.y);
+	intensity.z = mxmaxf(intensity.z, al.z);
 	return (intensity);
 }
