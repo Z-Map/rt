262811f8e5275fe7af6afaeb53dd449bf25d519c
Gabriel HUBERT <ghubert@e3r12p18.42.fr>
2017-07-02 17:01:21 +0200

    norme

--- src/raytracing/geometry_tool.c
+++ src/raytracing/geometry_tool.c
@@ -89,12 +89,12 @@ t_v2f		geo_uv(t_rtrd *rd)
 	return (uv);
 }
 
-t_v3f			calc_refraction(t_rtrgd gd, float ref)
+t_v3f		calc_refraction(t_rtrgd gd, float ref)
 {
-	float		n;
-	float		c1;
-	float		c2;
-	float		coef;
+	float	n;
+	float	c1;
+	float	c2;
+	float	coef;
 
 	n = 1.0f / ref;
 	c1 = v3fdotv3f(gd.hit_nor, gd.ray.direction);
